e603e91b237b1463ab763c1eceab4eaa093418b8
Pavao Begovac <pavaobegi@gmail.com>
2022-11-03 19:20:22 +0100

    adding startGame for player1

--- pregame.js
+++ pregame.js
@@ -1,15 +1,34 @@
-const createGame = document.querySelector(".header");
-const joinGame = document.querySelector(".card");
-const leaveGame = document.querySelector(".leaveGame");
-const nickname = document.querySelector("#name");
-const players = document.querySelector(".players");
-
-//if clicks on joingame without filling a form - popup you must set a nickname
-
 let startGame = () => {
-  joinGame.addEventListener("click", () => {
-    location.href = "table.html";
+  const firstPage = document.querySelector(".firstPage");
+  const makeRoom = document.querySelector(".playColors");
+  const nickname = document.querySelector("#nickname");
+  const player1 = document.querySelector("#player1");
+  const form = document.querySelector("#form");
+
+  const body2 = document.querySelector(".body");
+
+  nickname.focus();
+  body2.style.display = "none";
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    player1.innerHTML = nickname.value;
+    nickname.value = "";
+  });
+
+  //if clicks on makeRoom without filling a form - popup you must set a nickname
+  makeRoom.addEventListener("click", () => {
+    firstPage.style.display = "none";
+    //ask for what kind of game first player wants
+    body2.style.display = "flex";
   });
 };
 
+let joinGame = () => {
+  const player2 = document.querySelector("#player2");
+  const player3 = document.querySelector("#player3");
+  const player4 = document.querySelector("#player4");
+};
+
 startGame();
+joinGame();
