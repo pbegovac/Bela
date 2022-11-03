e8b51fa403ecb3979009cfc4d4eeef678a11682c
Pavao Begovac <pavaobegi@gmail.com>
2022-11-03 22:41:11 +0100

    adding room.js

--- pregame.js
+++ pregame.js
@@ -1,38 +1,27 @@
-let startGame = () => {
-  const firstPage = document.querySelector(".firstPage");
-  const makeRoom = document.querySelector(".playColors");
-  const nickname = document.querySelector("#nickname");
-  const player1 = document.querySelector("#player1");
-  const form = document.querySelector("#form");
-  const body2 = document.querySelector(".body");
+export const firstPage = document.querySelector(".firstPage");
+const makeRoom = document.querySelector(".playColors");
+const nickname = document.querySelector("#nickname");
+export const player1 = document.querySelector("#player1");
+const form = document.querySelector("#form");
+export const body2 = document.querySelector(".body");
 
-  nickname.focus();
-  body2.style.display = "none";
+nickname.focus();
+body2.style.display = "none";
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    player1.innerHTML = nickname.value;
-    //if clicks on makeRoom without filling a form - popup you must set a nickname
-  });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  player1.innerHTML = nickname.value;
+  //if clicks on makeRoom without filling a form - popup you must set a nickname
+});
 
-  makeRoom.addEventListener("click", () => {
-    if (nickname.value !== "") {
-      firstPage.style.display = "none";
-      //popup for what kind of game first player wants
+makeRoom.addEventListener("click", () => {
+  if (nickname.value !== "") {
+    firstPage.style.display = "none";
+    //popup for what kind of game first player wants
 
-      //display body2 when confirming the type of game
-      body2.style.display = "flex";
-    } else {
-      alert("Set Nickname");
-    }
-  });
-};
-
-let joinGame = () => {
-  const player2 = document.querySelector("#player2");
-  const player3 = document.querySelector("#player3");
-  const player4 = document.querySelector("#player4");
-};
-
-startGame();
-joinGame();
+    //display body2 when confirming the type of game
+    body2.style.display = "flex";
+  } else {
+    alert("Set Nickname");
+  }
+});
---
+++ room.js
@@ -0,0 +1,6 @@
+import { firstPage, player1, body2 } from "./pregame.js";
+
+const joinGame = document.querySelector(".card");
+const player2 = document.querySelector("#player2");
+const player3 = document.querySelector("#player3");
+const player4 = document.querySelector("#player4");
